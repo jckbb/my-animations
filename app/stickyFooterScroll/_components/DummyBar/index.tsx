b385f7aaeea1cbc79367ee00a31ff83433b100b0
Joseph Burghard <josephburgharddev@gmail.com>
2024-07-22 16:20:39 -0400

    add dummy bar component

---
+++ app/stickyFooterScroll/_components/DummyBar/index.tsx
@@ -0,0 +1,19 @@
+import { LayoutChangeEvent, View } from "react-native";
+
+interface Props {
+  onLayout: (e: LayoutChangeEvent) => void;
+}
+
+const DummyBar = ({ onLayout }: Props) => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: 50,
+      }}
+      onLayout={onLayout}
+    />
+  );
+};
+
+export default DummyBar;
