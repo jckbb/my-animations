d8e722363b36ae2c40ccf8d1205320e700b100ce
Joseph Burghard <josephburgharddev@gmail.com>
2024-07-23 13:59:39 -0400

    abstract component into dummy button

--- app/stickyFooterScroll/_components/Footer/index.tsx
+++ app/stickyFooterScroll/_components/Footer/index.tsx
@@ -1,73 +1,84 @@
 import { Animated, Text, View } from "react-native";
-import { InfoIcon } from "@/assets/icons";
 import ShareButton from "../ShareButton";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import styles from "./styles";
 
 interface Props {
   scrollY: Animated.Value;
   topEdge: number;
 }
 
-const Footer = ({ scrollY, topEdge }: Props) => {
-  const inset = useSafeAreaInsets();
+interface DummyButtonProps {
+  topEdge: number;
+  scrollY: Animated.Value;
+  label: string;
+  styling?: any;
+}
 
-  return (
-    <Animated.View
-      style={{
-        position: "absolute",
-        paddingHorizontal: 16,
-        bottom: inset.bottom,
-        left: 0,
-        right: 0,
-        backgroundColor: "white",
-        height: 50,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
+const DummyButton = ({
+  topEdge,
+  scrollY,
+  label,
+  styling,
+}: DummyButtonProps) => (
+  <Animated.View
+    style={[
+      {
         shadowColor: "#000",
         shadowOffset: {
           width: 0,
           height: 2,
         },
-        shadowRadius: 3.84,
-        elevation: 5,
-        transform: [
-          {
-            // move interpolated position at topedge
-            translateY: scrollY.interpolate({
-              inputRange: [-1, 0, topEdge - 1, topEdge],
-              outputRange: [0, 0, 0, -1],
-            }),
-          },
-        ],
-      }}
+        shadowOpacity: scrollY.interpolate({
+          inputRange: [-1, 0, topEdge - 1, topEdge],
+          outputRange: [0, 0, 0, 0.25],
+          extrapolate: "clamp",
+        }),
+        height: 40,
+        width: 40,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+        borderRadius: 20,
+      },
+      styling,
+    ]}
+  >
+    <Text>{label}</Text>
+  </Animated.View>
+);
+
+const Footer = ({ scrollY, topEdge }: Props) => {
+  const inset = useSafeAreaInsets();
+
+  return (
+    <Animated.View
+      style={[
+        styles.footer,
+        {
+          bottom: inset.bottom,
+          transform: [
+            {
+              // move interpolated position at topedge
+              translateY: scrollY.interpolate({
+                inputRange: [-1, 0, topEdge - 1, topEdge],
+                outputRange: [0, 0, 0, -1],
+              }),
+            },
+          ],
+        },
+      ]}
     >
-      <View style={{ flexDirection: "row" }}>
-        <View
-          style={{
-            height: 40,
-            width: 40,
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#7bed9f",
-            borderRadius: 20,
-          }}
-        >
-          <Text>{"+"}</Text>
-        </View>
+      <View style={styles.group}>
+        <DummyButton
+          styling={{ backgroundColor: "#7bed9f" }}
+          label="+"
+          topEdge={topEdge}
+          scrollY={scrollY}
+        />
         <ShareButton topEdge={topEdge} scrollY={scrollY} />
       </View>
-      <View
-        style={{
-          height: 40,
-          minWidth: 40,
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: 20,
-        }}
-      >
-        <InfoIcon height={30} width={30} />
-      </View>
+      <DummyButton label="i" topEdge={topEdge} scrollY={scrollY} />
     </Animated.View>
   );
 };
